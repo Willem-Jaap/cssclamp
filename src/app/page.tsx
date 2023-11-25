0c7bb73de616def1b61c65801fe18c7f02000252
Willem-Jaap <willemjaapk@gmail.com>
2023-11-25 18:41:43 +0100

    feat: add initial page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
 const Page = () => {
-    return <h1>CSS Clamp</h1>;
+    return (
+        <div className="flex flex-col md:flex-row gap-4 md:gap-8">
+            <div className="border border-neutral-800 rounded-xl flex-1">
+                <span className="block p-4">Preview</span>
+            </div>
+            <div className="flex flex-col justify-between border border-neutral-800 rounded-xl h-[calc(100vh-16rem)] md:max-w-sm flex-[0.4] overflow-hidden">
+                <span className="block p-4">
+                    Actions <span className="text-neutral-600">(not implemented yet)</span>
+                </span>
+            </div>
+        </div>
+    );
 };
 
 export default Page;
