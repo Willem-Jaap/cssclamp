b66a1a0fd239544350360bb48b85ece3d98aaa90
Willem-Jaap <willemjaapk@gmail.com>
2023-11-26 17:40:39 +0100

    chore: add shadcn configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,7 +35,21 @@ module.exports = {
                 sans: ['var(--font-satoshi)'],
                 mono: ['var(--font-roboto-mono)'],
             },
+            keyframes: {
+                'accordion-down': {
+                    from: { height: 0 },
+                    to: { height: 'var(--radix-accordion-content-height)' },
+                },
+                'accordion-up': {
+                    from: { height: 'var(--radix-accordion-content-height)' },
+                    to: { height: 0 },
+                },
+            },
+            animation: {
+                'accordion-down': 'accordion-down 0.2s ease-out',
+                'accordion-up': 'accordion-up 0.2s ease-out',
+            },
         },
     },
-    plugins: [],
+    plugins: [require('tailwindcss-animate')],
 };
