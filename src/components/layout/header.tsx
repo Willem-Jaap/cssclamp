8c0ba177058e2ed8fe1d18978a068c6b2832d2c3
Willem-Jaap <willemjaapk@gmail.com>
2023-12-10 14:41:39 +0100

    feat: add nav to header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,10 +4,28 @@ import Logo from '~/components/misc/logo';
 
 const Header = () => {
     return (
-        <header className="flex rounded-xl border border-neutral-800 px-8 py-4">
+        <header className="flex items-center justify-between gap-4 rounded-xl border border-neutral-800 py-4 pl-8 pr-4">
             <Link href="/">
                 <Logo />
             </Link>
+            <nav>
+                <menu className="flex">
+                    <li>
+                        <Link
+                            href="/guide"
+                            className="rounded-lg px-4 py-2 transition-colors duration-200 hover:bg-neutral-800">
+                            Guide
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href="/deepdive"
+                            className="rounded-lg px-4 py-2 transition-colors duration-200 hover:bg-neutral-800">
+                            Deepdive
+                        </Link>
+                    </li>
+                </menu>
+            </nav>
         </header>
     );
 };
