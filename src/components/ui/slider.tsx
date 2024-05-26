08d625da313e4109a383c75f028ba1461a8d0a7a
Willem-Jaap <willemjaapk@gmail.com>
2024-05-26 12:58:56 +0200

    feat: update slider styling

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -13,10 +13,10 @@ const Slider = forwardRef<
         ref={ref}
         className={cn('relative flex w-full touch-none select-none items-center', className)}
         {...props}>
-        <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-neutral-100 dark:bg-neutral-800">
-            <SliderPrimitive.Range className="absolute h-full bg-neutral-900 dark:bg-neutral-50" />
+        <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-neutral-100">
+            <SliderPrimitive.Range className="absolute h-full rounded-full bg-neutral-600" />
         </SliderPrimitive.Track>
-        <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-neutral-900 bg-white ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-50 dark:bg-neutral-950 dark:ring-offset-neutral-950 dark:focus-visible:ring-neutral-300" />
+        <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border border-neutral-300 bg-white ring-offset-white transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 " />
     </SliderPrimitive.Root>
 ));
 Slider.displayName = SliderPrimitive.Root.displayName;
